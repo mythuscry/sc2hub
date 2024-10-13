a4dd30b704f6e9c597a088fb4f74549fff4d4749
mythuscry <245146554@qq.com>
2024-10-13 11:20:45 +0800

    图片载入到canvas当中

--- src/views/Paint/draw.vue
+++ src/views/Paint/draw.vue
@@ -3,7 +3,7 @@
     class="flex-3 flex flex-direction-row w-100% h-100% min-h-screen bg-gray-300"
   >
     <div class="flex w-4/5 h-100% content-center justify-center">
-      <canvas ref="RefCanvas" class="w-4/5 bg-red-100"></canvas>
+      <canvas ref="canvasRef" class="w-4/5 bg-black"></canvas>
     </div>
     <div class="w-1/5 h-100%"></div>
   </div>
@@ -12,30 +12,60 @@
 <script setup lang="ts">
 import { path } from "@tauri-apps/api";
 import { defineComponent, onMounted, ref } from "vue";
-import image from "../../statics/images/maps/de_dust2.png"; // 假设你的assets目录在src目录下
+import MapPath from "../../statics/images/maps/de_dust2.png";
+// 获取画布对象
+const canvasRef = ref<HTMLCanvasElement | null>(null);
 
-defineComponent({
-  setup() {
-    const canvasRef = ref<HTMLCanvasElement | null>(null);
+let defaultScale = 1; // 默认缩放倍数
+var isMouseDown = false; // 是否处于鼠标拖动状态
 
-    onMounted(() => {
-      if (canvasRef.value) {
-        const ctx = canvasRef.value.getContext("2d");
-        if (ctx) {
-          const img = new Image();
-          img.onload = () => {
-            ctx.drawImage(img, 0, 0); // 将图片绘制到canvas上
-          };
-          img.src = image; // 设置图片源为引入的图片
-        }
-      }
-    });
+onMounted(() => {
+  const canvas = canvasRef.value;
+  if (!canvas) {
+    console.error("Canvas element not found");
+    return;
+  }
 
-    return {
-      canvasRef,
-    };
-  },
+  // 获取 2D 绘图上下文
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) {
+    console.error("Unable to get 2D context");
+    return;
+  }
+
+  // 示例绘制：绘制一个蓝色矩形
+  ctx.fillStyle = "blue";
+  ctx.fillRect(10, 10, 100, 100);
+
+  //   载入图片资源
+  const img = new Image();
+  img.src = MapPath;
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+
+  // 等待图片加载完
+  img.onload = () => {
+    // 图片加载完成后执行的逻辑
+    console.log("图片加载完成");
+
+    drawImageOnCanvas(ctx, img);
+  };
 });
+
+// 封装 drawImage 方法
+function drawImageOnCanvas(
+  ctx: CanvasRenderingContext2D,
+  img: HTMLImageElement
+) {
+  const canvas = ctx.canvas;
+  // 根据图片调整 canvas 的大小
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+
+  // 绘制图片
+  ctx.drawImage(img, 0, 0);
+}
 </script>
 
 <style scoped>
