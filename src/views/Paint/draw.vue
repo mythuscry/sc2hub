c1fe4182ae2af2db798c3e151ef4dfcf3bc5ce47
mythuscry <mythuscry@gmail.com>
2024-10-17 16:35:50 +0800

    调整canva对浏览器缩放的兼容性问题

--- src/views/Paint/draw.vue
+++ src/views/Paint/draw.vue
@@ -1,9 +1,7 @@
 <template>
-  <div
-    class="flex-3 flex flex-direction-row w-100% h-100% min-h-screen bg-gray-300"
-  >
-    <div class="flex w-4/5 h-100% content-center justify-center">
-      <canvas ref="canvas" class="w-4/5 bg-red-100"></canvas>
+  <div class="flex-3 flex flex-direction-row w-full h-full min-h-screen bg-white">
+    <div class="flex w-4/5 h-full content-center justify-center">
+      <canvas ref="canvasRef" class=" bg-black "></canvas>
     </div>
     <div class="w-1/5 h-100%"></div>
   </div>
@@ -11,37 +9,72 @@
 
 <script setup lang="ts">
 
-import image from "../../statics/images/maps/de_dust2.png"; // 假设你的assets目录在src目录下
-
-import { onMounted, ref, Ref } from 'vue';
-
-const canvas: Ref<HTMLCanvasElement | null> = ref(null);
+import { onMounted, ref } from "vue";
+import MapPath from "../../statics/images/maps/de_dust2.png";
+// 获取画布对象
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const dpr = window.devicePixelRatio;
 
 onMounted(() => {
-  if (canvas.value) {
-    const ctx = canvas.value.getContext('2d');
-    if (ctx) {
-      drawCanvas(ctx); // 绘制图形
-    }
+  const canvas = canvasRef.value;
+  if (!canvas) {
+    console.error("Canvas element not found");
+    return;
   }
+
+  // 获取 2D 绘图上下文
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) {
+    console.error("Unable to get 2D context");
+    return;
+  }
+
+  //   载入图片资源
+  const img = new Image();
+  img.src = MapPath;
+  canvas.width = window.innerHeight;
+  canvas.height = window.innerWidth;
+
+  canvas.style.width = '1024px';
+  canvas.style.height = '1024px';
+
+  // 等待图片加载完
+  img.onload = () => {
+    // 图片加载完成后执行的逻辑
+    console.log("图片加载完成");
+
+    drawImageOnCanvas(ctx, img);
+  };
 });
 
-function drawCanvas(ctx: CanvasRenderingContext2D): void {
-  // 清除整个 canvas
-  ctx.clearRect(0, 0, canvas.value!.width, canvas.value!.height);
 
-  // 绘制背景颜色
-  ctx.fillStyle = '#f0f0f0';
-  ctx.fillRect(0, 0, canvas.value!.width, canvas.value!.height);
-
-  // 绘制一个蓝色矩形
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(50, 50, 100, 100);
-
+function drawImageOnCanvas(
+  ctx: CanvasRenderingContext2D,
+  img: HTMLImageElement
+) {
+  const canvas = ctx.canvas;
+  ctx.scale(dpr, dpr);
+  // 根据图片调整 canvas 的大小
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
 
 
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // 绘制图片
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+
+  console.log("img w" + img.naturalWidth);
+  console.log("img h" + img.naturalHeight);
+  console.log("canvas w" + canvas.width);
+  console.log("canvas h" + canvas.height);
 
 }
+
+
+
 
 
 </script>
@@ -51,7 +84,10 @@ function drawCanvas(ctx: CanvasRenderingContext2D): void {
 .flex-3 {
   flex: 3;
 }
+
 .flex-4 {
   flex: 4;
 }
+
+
 </style>
